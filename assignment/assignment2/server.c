2a3e69a92f0b03781a1e4ffa24ea96ba78fa219c
seungwoo lee <timosean@naver.com>
2022-05-08 11:15:00 +0900

    2nd

---
+++ assignment/assignment2/server.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#define BUFFER_SIZE 100
+#define NP_SEND "./server_to_client"
+#define NP_RECEIVE "./client_to_server"
+
+int main(void) {
+	char receive_msg[BUFFER_SIZE], send_msg[BUFFER_SIZE];
+	int receive_fd, send_fd;
+	int value;
+
+	/*---------------------------------------*/
+	/* TODO 3 : make pipes and init fds      */
+	/* (1) make NP_RECEIVE pipe              */
+	/* (2) make NP_SEND pipe                 */
+	/* (3) init receive_fd and send_fd       */
+
+	//기존 named_pipe 제거
+	if(access(NP_RECEIVE, F_OK) == 0) {
+		unlink(NP_RECEIVE);
+	}
+	if(access(NP_SEND, F_OK) == 0) {
+		unlink(NP_SEND);
+	}
+
+	//접근권한이 0666인 named_pipe 생성
+	if(mkfifo(NP_RECEIVE, 0666) == -1) return -1;
+	if(mkfifo(NP_SEND, 0666) == -1) return -1;
+				
+	if((receive_fd = open(NP_RECEIVE, O_RDWR)) == -1) return -1;
+	if((send_fd = open(NP_SEND, O_RDWR)) == -1) return -1;
+	/* TODO 3 : END                          */
+	/*---------------------------------------*/
+
+	while (1) {
+		/*---------------------------------------*/
+		/* TODO 4 : read msg                     */
+		
+		if(read(receive_fd, receive_msg, sizeof(receive_msg)) == -1) return -1;
+
+		/* TODO 4 : END                          */
+		/*---------------------------------------*/
+
+		printf("server : receive %s\n", receive_msg);
+
+		value = atoi(receive_msg);
+
+		sprintf(send_msg, "%d", value*value);
+		printf("server : send %s\n", send_msg);
+
+		/*---------------------------------------*/
+		/* TODO 5 : write msg                    */
+
+		if(write(send_fd, send_msg, sizeof(send_msg)) == -1) return -1;
+
+		/* TODO 5 : END                          */
+		/*---------------------------------------*/
+	}
+	return 0;
+}
